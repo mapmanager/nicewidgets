5601e6678fb641507951fba7fc81f77f0d789186
Robert Cudmore <robert.cudmore@gmail.com>
2026-02-13 12:09:32 -0500

    before upgrade to plot pool controller

--- src/nicewidgets/utils/__init__.py
+++ src/nicewidgets/utils/__init__.py
@@ -1,8 +1,10 @@
 """Utility functions for nicewidgets."""
 
+from .gui_defaults import setUpGuiDefaults
 from .logging import get_logger, get_log_file_path, setup_logging
 
 __all__ = [
+    "setUpGuiDefaults",
     "get_logger",
     "get_log_file_path",
     "setup_logging",
